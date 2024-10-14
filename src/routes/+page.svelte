a95d88230edc74e6038dbac7a0af49e922c870bd
Jacob Miles <jacob.e.miles@pm.me>
2024-10-13 22:26:05 -0500

    testing colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 
 <script></script>
 
-<section>
+<section class="bg-[#FCF5E5] min-h-[100vh]">
   <div class="flex flex-col justify-center items-center">
-    <span class="text-2xl text-green-700">Mt. Pleasant Community Orchistra Presents</span>
+    <span class="text-2xl text-green-700">Mt. Pleasant Community Orchistra</span>
+    <span class="text-2xl text-green-700">Presents</span>
     <span class="text-3xl text-red-700">Christmas Through the Ages</span>
   </div>
 </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,56 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const plugin = require('tailwindcss/plugin')
+const colors = require('tailwindcss/colors')
+const forms = require('@tailwindcss/forms')
+
+const config = {
+  mode: 'jit',
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    // extend: {
+    fontFamily: {
+      nunito: ['nunito']
+    },
+    // },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      lightBackgroundBlue: '#dfe5ed',
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      black: colors.black,
+      white: colors.white,
+      cgreen: {
+        light: '#165b33',
+        dark: '#146b3a',
+        DEFAULT: '#165b33'
+      },
+      orange_yellow: '#f8b229',
+      cinnabar: '#ea4630',
+      firebrick: '#bb2528'
+    }
   },
-  plugins: [],
+  plugins: [forms]
 }
+
+module.exports = config
 
