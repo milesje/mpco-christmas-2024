98f0f96ddb925690a2d178b644d7a877f8944223
Jacob Miles <jacob.e.miles@pm.me>
2024-10-13 19:49:19 -0500

    initial changes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+
+<script></script>
+
+<section>
+  <div class="flex flex-col justify-center items-center">
+    <span class="text-2xl text-green-700">Mt. Pleasant Community Orchistra Presents</span>
+    <span class="text-3xl text-red-700">Christmas Through the Ages</span>
+  </div>
+</section>
